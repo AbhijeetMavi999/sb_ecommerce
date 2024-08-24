00d7021bd19ac9444aa02c33a04b7cf8a4307b44
Abhijeet Mavi <abhijeetmavi999@gmail.com>
2024-08-24 10:19:52 +0530

    initial commit

---
+++ src/main/java/com/sb_ecom/SbEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.sb_ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb_ecom/SbEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
